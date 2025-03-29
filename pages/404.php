6f227141e536bf5d183e2e021c889810d954e939
ngbaduyy10 <ngbaduyy05@gmail.com>
2025-03-29 15:08:18 +0700

    404 page implemented

--- pages/404.php
+++ pages/404.php
@@ -1,1 +1,48 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="assets/css/reset.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;0,700;1,400&family=Sen:wght@400..800&display=swap"
+        rel="stylesheet"
+    />
+    <link rel="stylesheet" href="assets/css/404.css" />
+    <title>404</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container not-found">
+    <!-- Main Content -->
+    <div class="row align-items-center">
+        <!-- Text Section -->
+        <div class="col-lg-6 text-center text-lg-start mb-4 mb-lg-0">
+            <h1 class="display-3 text-danger fw-bold">Oops!</h1>
+            <h2 class="text-dark mb-3">The page you are looking for cannot be found</h2>
+            <ul class="text-muted list-unstyled">
+                <li>The URL may have been typed incorrectly.</li>
+                <li>It may be a broken or outdated link.</li>
+            </ul>
+            <div class="d-flex justify-content-center justify-content-lg-start gap-3 mt-4">
+                <a href="?page=home" class="button button-primary">Back to Home</a>
+            </div>
+        </div>
+
+        <!-- Image Section -->
+        <div class="col-lg-6 text-center">
+            <img src="assets/image/404.jpg" alt="404 Illustration" class="img-fluid w-100">
+        </div>
+    </div>
+</div>
+
+<!--Bootstrap-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
+
