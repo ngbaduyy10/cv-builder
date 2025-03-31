1beef848970ec362100ec7f015bbfddd4b9f6fb7
ngbaduyy10 <ngbaduyy05@gmail.com>
2025-03-31 14:02:42 +0700

    template manage fixed

--- pages/admin/template-manage.php
+++ pages/admin/template-manage.php
@@ -19,7 +19,7 @@
     />
     <link rel="stylesheet" href="assets/css/admin/sidebar.css" />
     <link rel="stylesheet" href="assets/css/admin/template-manage.css" />
-    <title>User</title>
+    <title>Template</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
