b7f2c5981ce59ba5f5902ed906030627e3dfc814
ngbaduyy10 <ngbaduyy05@gmail.com>
2025-03-31 13:56:05 +0700

    cv-1 implemented

---
+++ template/cv-1.php
@@ -0,0 +1,66 @@
+<div class = "preview-cnt">
+    <div class = "preview-blk text-center">
+        <div class = "preview-item preview-item-name">
+            <span class = "preview-item-val fw-6" id = "fullname_dsp"></span>
+        </div>
+        <div class = "preview-item preview-item-job">
+            <span class = "preview-item-val" id = "job_dsp"></span>
+        </div>
+        <div class = "py-1 d-flex justify-content-center gap-3">
+            <span class = "preview-item-val" id = "phoneno_dsp"></span>
+            <span class = "preview-item-val" id = "email_dsp"></span>
+            <span class = "preview-item-val" id = "address_dsp"></span>
+        </div>
+    </div>
+
+    <hr>
+
+    <div class = "preview-blk text-center">
+        <div class = "preview-blk-title">
+            <h3>Objectives</h3>
+        </div>
+        <div class = "preview-item preview-item-objective">
+            <span class = "preview-item-val" id = "summary_dsp"></span>
+        </div>
+    </div>
+
+    <hr>
+
+    <div class = "preview-blk">
+        <div class = "preview-blk-title text-center">
+            <h3>experiences</h3>
+        </div>
+        <div class = "experiences-items preview-blk-list" id = "experiences_dsp"></div>
+    </div>
+
+    <div class = "preview-blk">
+        <div class = "preview-blk-title text-center">
+            <h3>projects</h3>
+        </div>
+        <div class = "projects-items preview-blk-list" id = "projects_dsp"></div>
+    </div>
+
+    <div class = "preview-blk">
+        <div class = "preview-blk-title text-center">
+            <h3>educations</h3>
+        </div>
+        <div class = "educations-items preview-blk-list" id = "educations_dsp"></div>
+    </div>
+
+    <div class = "preview-blk text-center">
+        <div class = "preview-blk-title">
+            <h3>skills</h3>
+        </div>
+        <div class = "skills-items preview-blk-list" id = "skills_dsp">
+            <!-- skills list here -->
+        </div>
+    </div>
+
+    <div class = "preview-blk">
+        <div class = "preview-blk-title text-center">
+            <h3>Achievements</h3>
+        </div>
+        <div class = "achievements-items preview-blk-list" id = "achievements_dsp"></div>
+    </div>
+</div>
+
