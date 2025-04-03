d3340a5469070b48194327f191a4e921bd907eaf
ngbaduyy10 <ngbaduyy05@gmail.com>
2025-04-03 21:19:50 +0700

    my-cv design fixed

--- assets/js/client/my-cv.js
+++ assets/js/client/my-cv.js
@@ -9,38 +9,50 @@ $(document).ready(function () {
             },
             success: function (response) {
                 if (response.success) {
-                    response.data.forEach((cv) => {
-                        let cvHtml = `
-                            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
-                                <div class="cv-item p-4 h-100 d-flex flex-column">
-                                    <img src="${cv.preview_image}" alt="" class="w-100" />
-                                    <a href="?page=cv-creation&template_id=${cv.template_id}&cv_id=${cv.id}" class="button button-edit">
-                                        <i class='bx bxs-edit-alt'></i>
-                                    </a>
-                                    <button class="button button-delete" id="button-delete" data-cv-id="${cv.id}">
-                                        <i class='bx bx-trash'></i>
-                                    </button>
-                                    <div class="mt-3 detail">
-                                        <h3 class="title">${cv.cvname}</h3>
-                                        <div class="d-flex align-items-center gap-2">
-                                            <i class='bx bx-book-bookmark'></i>
-                                            <p class="desc">${cv.template_name}</p>
-                                        </div>
-                                        <div class="d-flex align-items-center gap-2">
-                                            <i class='bx bx-time-five'></i>
-                                            <p class="desc">Created at ${cv.created_at}</p>
-                                        </div>
-                                        <div class="d-flex align-items-center gap-2">
-                                            <i class='bx bx-cloud-upload' ></i>
-                                            <p class="desc">Last updated at ${cv.updated_at}</p>
+                    if (response.data.length === 0) {
+                        $('#cv-list').html(`
+                            <div class="col-12 notice">
+                                <div class="text-center">
+                                    <h4>No CVs Found</h4>
+                                    <p>You haven't created any CVs yet. Click the button below to create your first CV.</p>
+                                    <a href="?page=template" class="btn btn-primary">Create CV</a>
+                                </div>
+                            </div>
+                        `);
+                    } else {
+                        response.data.forEach((cv) => {
+                            let cvHtml = `
+                                <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+                                    <div class="cv-item p-4 h-100 d-flex flex-column">
+                                        <img src="${cv.preview_image}" alt="" class="w-100" />
+                                        <a href="?page=cv-creation&template_id=${cv.template_id}&cv_id=${cv.id}" class="button button-edit">
+                                            <i class='bx bxs-edit-alt'></i>
+                                        </a>
+                                        <button class="button button-delete" id="button-delete" data-cv-id="${cv.id}">
+                                            <i class='bx bx-trash'></i>
+                                        </button>
+                                        <div class="mt-3 detail">
+                                            <h3 class="title">${cv.cvname}</h3>
+                                            <div class="d-flex align-items-center gap-2">
+                                                <i class='bx bx-book-bookmark'></i>
+                                                <p class="desc">${cv.template_name}</p>
+                                            </div>
+                                            <div class="d-flex align-items-center gap-2">
+                                                <i class='bx bx-time-five'></i>
+                                                <p class="desc">Created at ${cv.created_at}</p>
+                                            </div>
+                                            <div class="d-flex align-items-center gap-2">
+                                                <i class='bx bx-cloud-upload' ></i>
+                                                <p class="desc">Last updated at ${cv.updated_at}</p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        `;
+                            `;
 
-                        $('#cv-list').append(cvHtml);
-                    })
+                            $('#cv-list').append(cvHtml);
+                        })
+                    }
                 }
             },
             error: function (xhr, status, error) {
