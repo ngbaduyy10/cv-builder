f37a709661387c1a7864f07a1456bebfe65a8c56
ngbaduyy10 <ngbaduyy05@gmail.com>
2025-04-08 12:18:30 +0700

    database config

--- config/database.php
+++ config/database.php
@@ -1,9 +1,16 @@
 <?php
 class Database {
-    private $host = "localhost";
-    private $db_name = "cv-builder";
-    private $username = "root";
-    private $password = "";
+    // Local database configuration
+//    private $host = "localhost";
+//    private $db_name = "cv-builder";
+//    private $username = "root";
+//    private $password = "";
+
+    // Remote database configuration
+    private $host = "p1us8ottbqwio8hv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    private $db_name = "p3016rq39iyk3zx6";
+    private $username = "y65vnfmbeqycr2t7";
+    private $password = "ufhwpj5gqcbj5hfb";
 
     public function connect() {
         try {
